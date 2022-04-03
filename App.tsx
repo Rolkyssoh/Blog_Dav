341a9164ee947f150039f7efb5af23341231b332
Rolkyssoh <juventussoh@gmail.com>
2022-04-03 13:16:46 +0000

    The first commit

---
+++ App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {HomeScreen} from './src/screens';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <HomeScreen />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ src/screens/base-component/base.tsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export interface IBaseProps {
+  default_props?: boolean;
+}
+
+export const BaseComponent: React.FC<IBaseProps> = () => {
+  return (
+    <View style={styles.theColor}>
+      <Text>Just the test</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  theColor: {
+    backgroundColor: 'red',
+    flex: 1,
+  },
+});
